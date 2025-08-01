c918fbabaa0a5f7b932aa4db59dd66bd5d0bf3f9
R.Nodankumar <reddynodankumar@gmail.com>
2025-08-01 19:39:16 +0530

    configured xml report in testng runner

--- src/test/java/org/grogu/runners/TestRunnerTestNG.java
+++ src/test/java/org/grogu/runners/TestRunnerTestNG.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/resources/features", glue = "org.grogu.steps",
-        plugin = {"pretty","html:target/reports/report.html"}
+        plugin = {"pretty","html:target/reports/report.html", "junit:target/reports/report.xml"}
 )
 public class TestRunnerTestNG extends AbstractTestNGCucumberTests {
 }
