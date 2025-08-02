a2bf74195139c1fa3fba89143bf2baf422576453
R.Nodankumar <reddynodankumar@gmail.com>
2025-08-02 15:58:54 +0530

    reports genertion for each runner

--- src/test/java/org/grogu/runners/TestRunnerJ4.java
+++ src/test/java/org/grogu/runners/TestRunnerJ4.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = "org.grogu.steps",
-        plugin = {"pretty","html:target/reports/report.html", "junit:target/reports/report.xml",
+        plugin = {"pretty","html:target/reports/report.html", "junit:target/reports/reportj4.xml",
         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         }
 )
